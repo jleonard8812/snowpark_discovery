75ad99b97d1a67ce9f67e430a99e585d07917e8d
John Leonard <48164195+jleonard8812@users.noreply.github.com>
2024-04-13 07:38:55 -0600

    Update Connection_test.py

--- Connection_test.py
+++ Connection_test.py
@@ -6,7 +6,7 @@ from snowflake.snowpark.functions import col
 
 connection_parameters = {"account":"BDUHNMX-SD34895",
 "user":"jleonard8812",
-"password": "Lilthuglife8812!!!",
+"password":
 "role":"ACCOUNTADMIN",
 "warehouse":"COMPUTE_WH",
 "database":"AZURE_TEST",
